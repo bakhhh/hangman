f8f4c153383e5012d2caaf459faf3e101d81ebad
Sohail Bakhshi <sohailb@Sohails-MBP.modem>
2023-02-11 23:02:44 +0800

    Fixed up the layout

--- hangman.c
+++ hangman.c
@@ -21,35 +21,36 @@ int main() {
     srand(time(NULL));
     int randomIndex = rand() % 10;
 
-    printf("Welcome to hangman\n");
-    printf("*CLUE* %s\n",words[randomIndex].characteristic);
+    printf("\nWelcome to hangman\n");
+    printf("\n*CLUE* %s\n",words[randomIndex].characteristic);
     
 
     while (incorrect_guesses < 7 && correct_guesses < strlen(words[randomIndex].word) && (strcmp(guess, words[randomIndex].word) != 0)){
-        printf("Would you like to guess the word [YES][NO]: ");
+        printf("\ncorrect guesses %d/%lu                             lives %d/7\n",correct_guesses,strlen(words[randomIndex].word),incorrect_guesses);
+        printf("\nWould you like to guess the word [YES][NO]: ");
         scanf("%s", option);
 
         if (strcmp(option, "yes") == 0) {
-            printf("Enter your guess: ");
+            printf("\nEnter your guess: ");
             scanf(" %s", guess);
             
             if (strcmp(guess, words[randomIndex].word) == 0) {
-                printf("Congratulations! You guessed the word correctly.\n");
+                printf("\nCongratulations! You guessed the word correctly.\n");
                 break;
             }
             else if (strcmp(guess, words[randomIndex].word) != 0){
-                printf("Your guess is  incorrect.\n");
+                printf("\nYour guess is incorrect.\n");
                 incorrect_guesses+=2;
             }
         }
         else if (strcmp(option, "no") == 0) {
-            printf("Enter your letter guess: ");
+            printf("\nEnter your letter guess: ");
             scanf(" %c", &letter);
 
             found = false;
             for (int i = 0; i < strlen(words[randomIndex].word); i++) {
                 if ((words[randomIndex].word)[i] == letter) {
-                    printf("The letter '%c' was found in the word.\n", letter);
+                    printf("\nThe letter '%c' was found in the word.\n", letter);
                     found = true;
                     correct_guesses++;
                     break;
@@ -57,14 +58,14 @@ int main() {
             }
 
             if (found == false) {
-                printf("The letter '%c' was not found in the word.\n", letter);
+                printf("\nThe letter '%c' was not found in the word.\n", letter);
                 incorrect_guesses ++;
             }
         }
     }
 
     if (incorrect_guesses >= 7) {
-        printf("\nUnlucky You Lose\n");
+        printf("\nYou Lose the word was %s\n",words[randomIndex].word);
     }
 
     if (correct_guesses == strlen(words[randomIndex].word)) {
